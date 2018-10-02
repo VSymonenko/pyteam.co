f65328c653b843ae544f9f8781daecff61dd909b
VSymonenko <simonenko744@gmail.com>
2018-10-02 17:05:18 +0300

    init app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <!-- TODO: add router view -->
+      <MainContent/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span :style="{ padding: '10px'}">&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import MainContent from './components/MainContent'
+
+export default {
+  name: 'App',
+  components: {
+    MainContent
+  },
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'PyTeam'
+    }
+  }
+}
+</script>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode='out-in'>
+      <v-layout column align-center>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'main-content',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
