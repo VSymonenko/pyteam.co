d11750b56c41a499ad2ffe3cfda31a577704b725
VSymonenko <simonenko744@gmail.com>
2018-11-13 17:50:10 +0200

    :eyes: add basic theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,14 @@ import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
+  theme: {
+    primary: '#9E9E9E',
+    secondary: '#80D8FF',
+    accent: '#9c27b0',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#2196f3',
+    success: '#4caf50',
+    additional: '#777777',
+  },
 });
